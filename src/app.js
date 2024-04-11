72572835dfbb6ec50a8c6184b3dd4badcdcda972
feketejanosdavid <janosdavidfekete@gmail.com>
2024-04-11 10:34:18 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const employeeArray = [
+    {id:1, name:"Erős István", city: "Szeged", salary: 365},
+    {id:2, name:"Gyenge Péter", city: "Szolnok", salary: 165},
+    {id:3, name:"Fehér Anita", city: "Budapest", salary: 65},
+    {id:4, name:"Prof. Dr. Sr. James Michael-Wood", city: "London", salary: 12300},
+    {id:5, name:"Ifj. Erős István", city: "Szeged", salary: 465}
+] 
+
+const empsBody = document.getElementById("empsBody")
+
+employeeArray.forEach( (employee) => {
+    console.log(emp.name)
+
+    var row = ``
+})
